40f9180053b312710add4246990bea6c7aea0018
jordan <jchan1e@gmail.com>
2015-09-23 02:22:00 -0600

    added Galilean Moons to HW3, cleaned up extraneous binaries in the examples folder

--- SimpleScene/objects.c
+++ SimpleScene/objects.c
@@ -15,4 +15,152 @@
 
 #endif
 
-// 
+//Cos and Sin in degrees - stolen from ex8
+#define Sin(x) (sin((x) * 3.1415927/180))
+#define Cos(x) (cos((x) * 3.1415927/180))
+
+//Vertex Polar-Cartesian tranformation function also pilfered from ex8 
+void Vertex(double th, double ph)
+{
+   glColor3f(fabs(Cos(th)*Cos(ph)), fabs(Sin(th)*Cos(ph)), fabs(Sin(ph)));
+   glVertex3d(Sin(th)*Cos(ph), Sin(ph), Cos(th)*Cos(ph));
+}
+
+//without the coloration
+void Vertex2(double th, double ph)
+{
+   glVertex3d(Sin(th)*Cos(ph), Sin(ph), Cos(th)*Cos(ph));
+}
+
+//Sphere function nabbed from ex8 and slightly modified
+void sphere(double x, double y, double z,
+            double r,
+            double s)
+{
+   glPushMatrix();
+
+   glTranslated(x, y, z);
+   glRotated(r, 0,1,0);
+   glScaled(s, s, s);
+
+   //top fan
+   glBegin(GL_TRIANGLE_FAN);
+   Vertex(0,90);
+   for(int th = 0; th <= 360; th += 5)
+   {
+      Vertex(th, 85);
+   }
+   glEnd();
+
+   //latitude rings
+   for (int ph = 85; ph >= -80; ph -= 5)
+   {
+      glBegin(GL_QUAD_STRIP);
+      for (int th = 0; th <= 360; th += 5)
+      {
+         Vertex(th, ph);
+         Vertex(th, ph-5);
+      }
+      glEnd();
+   }
+   
+   //bottom fan
+   glBegin(GL_TRIANGLE_FAN);
+   Vertex(0,-90);
+   for(int th = 360; th >= 0; th -= 5)
+   {
+      Vertex(th, -85);
+   }
+   glEnd();
+
+   glPopMatrix();
+}
+
+void icosahedron(double x, double y, double z,
+                 double r,
+                 double s)
+{
+   double lat = atan(0.5)*180/3.1415927;
+   int i = 0;
+   double points[12][2];
+   points[0][0] = 0;       points[0][1] = 90; 
+   points[1][0] = 0;       points[1][1] = lat; 
+   points[2][0] = 36;      points[2][1] = -lat; 
+   points[3][0] = 72;      points[3][1] = lat; 
+   points[4][0] = 108;     points[4][1] = -lat; 
+   points[5][0] = 144;     points[5][1] = lat; 
+   points[6][0] = 180;     points[6][1] = -lat; 
+   points[7][0] = 216;     points[7][1] = lat; 
+   points[8][0] = 252;     points[8][1] = -lat; 
+   points[9][0] = 288;     points[9][1] = lat; 
+   points[10][0]= 324;     points[10][1]= -lat; 
+   points[11][0]= 0;       points[11][1]= -90; 
+
+   glPushMatrix();
+   glTranslated(x, y, z);
+   glRotated(r, 0,1,0);
+   glScaled(s, s, s);
+
+   glColor3f(0.1,0.2,0.3);
+
+   //top 5 triangles
+   glBegin(GL_TRIANGLE_FAN);
+   Vertex2(points[0][0], points[0][1]);
+   for (i = 1; i < 10; i += 2)
+      Vertex2(points[i][0], points[i][1]);
+   Vertex2(points[1][0], points[1][1]);
+   glEnd();
+
+   //middle 10 triangles
+   glBegin(GL_TRIANGLE_STRIP);
+   for (i = 1; i <= 10; ++i)
+      Vertex2(points[i][0], points[i][1]);
+   Vertex2(points[1][0], points[1][1]);
+   Vertex2(points[2][0], points[2][1]);
+   glEnd();
+
+   //bottom 5 triangles
+   glBegin(GL_TRIANGLE_FAN);
+   Vertex2(points[11][0], points[11][1]);
+   for (i = 10; i > 1; i -= 2)
+      Vertex2(points[i][0], points[i][1]);
+   Vertex2(points[11][0], points[11][1]);
+   glEnd();
+
+   glColor3f(1,1,1);
+   glLineWidth(1.5);
+
+   //top fan of edges
+   glBegin(GL_LINES);
+   for (i = 1; i <= 9; i += 2)
+   {
+      Vertex2(points[0][0], points[0][1]);
+      Vertex2(points[i][0], points[i][1]);
+   }
+   glEnd();
+   //top ring of edges
+   glBegin(GL_LINE_STRIP);
+   for (i = 1; i <= 9; i += 2)
+      Vertex2(points[i][0], points[i][1]);
+   //middle zigzag edges
+   for (i = 1; i <= 10; ++i)
+      Vertex2(points[i][0], points[i][1]);
+   Vertex2(points[1][0], points[1][1]);
+   glEnd();
+   //bottom ring of edges
+   glBegin(GL_LINE_STRIP);
+   for (i = 2; i <= 10; i += 2)
+      Vertex2(points[i][0], points[i][1]);
+   Vertex2(points[2][0], points[2][1]);
+   glEnd();
+   //bottom fan of edges
+   glBegin(GL_LINES);
+   for (i = 2; i <= 10; i += 2)
+   {
+      Vertex2(points[11][0], points[11][1]);
+      Vertex2(points[i][0], points[i][1]);
+   }
+   glEnd();
+
+   glPopMatrix();
+}
--- SimpleScene/scene.c
+++ SimpleScene/scene.c
@@ -12,23 +12,118 @@
 #else
 #include <GL/glut.h>
 #endif
-
 #endif
+
+#include "objects.c"
+
+//GLOBAL VARIABLES//
+
+//View Angles
+int th = 0;
+int ph = 0;
+//Window Size
+int w = 500;
+int h = 500;
+
+//sphere rotation
+double r = 0;
+
+////////////////////
+
 
 void display()
 {
+   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+   glEnable(GL_DEPTH_TEST);
+
+   glLoadIdentity();
+   //view angle
+   glRotatef(ph, 1,0,0);
+   glRotatef(th, 0,1,0);
+   glScaled(0.4,0.4,0.4);
+
+   sphere(0, 0, 0, r, 0.5); //Jupiter
+   glPushMatrix();
+   glRotated(r/2, 0,1,0);
+   sphere(1, 0, 0, 1.5*r, 0.25); //IO
+   glPopMatrix();
+   glPushMatrix();
+   glRotated(r/4, 0,1,0);
+   sphere(-2, 0, 0, 4.0/3.0*r, 0.25); //Europa
+   glPopMatrix();
+   glPushMatrix();
+   glRotated(r/8, 0,1,0);
+   sphere(3, 0, 0, -1.125*r, 0.25); //Ganymede
+   glPopMatrix();
+   glPushMatrix();
+   glRotated(r/18.4, 0,1,0);
+   icosahedron(4, 0, 0, 0.5*r, 0.25); //Callisto
+   glPopMatrix();
+
+   r += 0.25;
+   r = fmod(r, 360*24*2.3);
+   glFlush();
+   glutSwapBuffers();
 }
 
 void reshape(int width, int height)
 {
+   w = width;
+   h = height;
+   //new aspect ratio
+   double w2h = (height > 0) ? (double)width/height : 1;
+   //set viewport to the new window
+   glViewport(0,0 , width,height);
+
+   //switch to projection matrix
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   //adjust aspect ratio
+   glOrtho(-2*w2h, 2*w2h, -2, 2, -2, 2);
+
+   //switch back to model matrix
+   glMatrixMode(GL_MODELVIEW);
+   glLoadIdentity();
 }
 
 void special(int key, int mousex, int mousey)
 {
+   switch(key)
+   {
+      case GLUT_KEY_UP:
+         ph += 5;
+         break;
+      case GLUT_KEY_DOWN:
+         ph -= 5;
+         break;
+      case GLUT_KEY_LEFT:
+         th += 5;
+         break;
+      case GLUT_KEY_RIGHT:
+         th -= 5;
+         break;
+   }
+   ph %= 360;
+   th %= 360;
 }
 
 void keyboard(unsigned char key, int mousex, int mousey)
 {
+   switch(key)
+   {
+      case 27: //escape
+         exit(0);
+         break;
+      case 'q':
+         exit(0);
+         break;
+   }
+   glutPostRedisplay();
+}
+
+void idle()
+{
+   glutPostRedisplay();
 }
 
 int main(int argc, char *argv[])
@@ -45,6 +140,9 @@ int main(int argc, char *argv[])
    glutSpecialFunc(special);
    glutKeyboardFunc(keyboard);
    //glutPassiveMotionFunc(motion);
+   glutIdleFunc(idle);
 
    glutMainLoop();
+
+   return EXIT_SUCCESS;
 }
